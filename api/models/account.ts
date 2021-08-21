2113e5fdb0a2e05301b81a0bb0117b7bfb82da3e
ascented <toffuyume@gmail.com>
2021-08-21 21:24:59 +0300

    [API] fixed account model

--- api/models/account.ts
+++ api/models/account.ts
@@ -5,10 +5,14 @@ import { database } from '../database';
 export const AccountModel = database.define('Account', {
     id: {
         type: DataTypes.INTEGER,
+        primaryKey: true,
         autoIncrement: true
     },
     name: {
         type: DataTypes.STRING(64),
+    },
+    password_hash: {
+        type: DataTypes.STRING(64)
     },
     api_token: {
         type: DataTypes.STRING(64)
