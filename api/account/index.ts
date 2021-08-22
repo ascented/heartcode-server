1195c09e5f4ab0fef6418db1c388ff6a08c90e2a
ascented <toffuyume@gmail.com>
2021-08-22 03:53:37 +0300

    [API] prettify, fixes, added get method, added pay method

--- api/account/index.ts
+++ api/account/index.ts
@@ -1,34 +1,57 @@
-import * as express from 'express';
 import { Controller, Get, Post, Requires, Request, Use } from '../controller';
-import { ErrorResponse } from '../typings';
 import { AccountModel } from '../models/account';
 import { generateToken, hashFrom } from '../crypto';
-import { User } from 'discord.js';
 import Security from '../security';
+import { FindOptions } from 'sequelize';
 
 
 export default class AccountController extends Controller {
     public static signUpSessions = new Map<string, boolean>();
-    public static authorized = new Map<string, number>(); // <api_token, id>
+    public static authorized = new Map<string, number>(); // <api_token, name>
 
     private static verifySignUpSession(request: Request) {
         let data = request.data;
-        let sessions = this.signUpSessions;
+        let sessions = AccountController.signUpSessions;
         if (!sessions.has(data.session_key) || sessions.get(data.session_key)) {
             return request.error({
-                code: 'signUp.invalidSession',
+                code: 'session.invalid',
                 message: 'Invalid session.'
             });
         }
     }
     
     private static verifyToken(request: Request) {
-        if (!this.authorized.get(request.data.api_token)) {
+        if (!AccountController.authorized.get(request.data.api_token)) {
             request.error({
-                code: 'auth.notAuthorized',
+                code: 'token.notAuthorized',
                 message: 'Not authorized.'
             });
         }
+    }
+
+    private static async select(options: FindOptions<any>): Promise<any | any[]> {
+        let query = await AccountModel.findAll(options);
+        if (!query.length) {
+            return null;
+        } else {
+            if (query.length > 1) {
+                return query;
+            } else {
+                return query[0];
+            }
+        }
+    }
+
+    /**
+     * Работает только с авторизованными пользователями.
+     */
+    private static async getOwn(request: Request) {
+        let account = await AccountController.select({
+            where: {
+                name: AccountController.authorized.get(request.data.api_token)
+            } 
+        });
+        return account;
     }
 
     @Post('/account/signUp/openSession')
@@ -53,28 +76,22 @@ export default class AccountController extends Controller {
         /**
          * Проверка на существования сессии и закрытость потока.
          */
-        if (!sessions.has(data.session_key) || sessions.get(data.session_key)) {
-            return request.error({
-                code: 'signUp.invalidSession',
-                message: 'Invalid session.'
-            });
-        }
         sessions[data.session_key] = true; // Закрытие потока
         if (data.name > 64 || !data.name) {
             return request.error({
-                code: 'validation.stringLength',
+                code: 'name.badLength',
                 message: 'Bad length for name. (3-64 symbols)'
             });
         }
         if (await AccountModel.count({ where: { name: data.name } })) {
             return request.error({
-                code: 'signUp.busyName',
+                code: 'name.busy',
                 message: 'This name is busy.'
             });
         }
         if (data.password.length < 6 || data.password.length > 512) {
             return request.error({
-                code: 'validation.passwordLength',
+                code: 'password.badLength',
                 message: 'Bad length for password. (6-512 symbols)'
             });
         }
@@ -92,23 +109,44 @@ export default class AccountController extends Controller {
     public static async login(request: Request) {
         let data = request.data;
         let passwordHash = hashFrom(data.password);
-        let accountQuery = await AccountModel.findAll({ where: { name: data.name } });
-        let account = accountQuery[0] as any;
+        let account = await AccountController.select({ where: { name: data.name } });
+        console.log(JSON.stringify(account));
         if (!account) {
             return request.error({
-                code: 'login.notExists',
+                code: 'name.notExists',
                 message: 'This account does not exists.'
             });
         }
         if (account.password_hash != passwordHash) {
             return request.error({
-                code: 'login.badPassword',
-                message: 'Bad password.'
+                code: 'password.invalid',
+                message: 'Invalid password.'
             });
         }
-        AccountController.authorized.set(account.api_token, account.id);
+        AccountController.authorized.set(account.api_token, account.name);
         return request.response({
             api_token: account.api_token
+        });
+    }
+
+    @Get('/account')
+    @Use(AccountController.verifyToken)
+    @Requires('name')
+    public static async get(request: Request) {
+        let data = request.data;
+        let account = await AccountController.select({ where: { name: data.name } });
+        if (!account) {
+            return request.error({
+                code: 'name.notExists',
+                message: 'This account does not exists.'
+            });
+        }
+        let { name, balance, rank_points, createdAt } = account;
+        return request.response({
+            name: name,
+            balance: balance,
+            rank_points: rank_points,
+            created_at: createdAt
         });
     }
 
@@ -119,12 +157,11 @@ export default class AccountController extends Controller {
         let data = request.data;
         if (data.name > 64 || !data.name) {
             return request.error({
-                code: 'validation.stringLength',
+                code: 'name.badLength',
                 message: 'Bad length for name. (3-64 symbols)'
             });
         }
-        let accountQuery = await AccountModel.findAll({ where: { api_token: data.api_token } });
-        let account = accountQuery[0] as any;
+        let account = await AccountController.select({ where: { api_token: data.api_token } });
         await account.update({
             name: data.name
         });
@@ -133,6 +170,36 @@ export default class AccountController extends Controller {
 
     @Post('/account/pay')
     @Use(AccountController.verifyToken)
-    @Requires('amount', 'api_token')
-    public static async pay(request: Request) {}
+    @Requires('amount', 'name')
+    public static async pay(request: Request) {
+        let data = request.data;
+        let amount = parseInt(data.amount);
+        let account = await AccountController.getOwn(request);
+        if (!amount || amount < 0 || amount > 2147483647) {
+            return request.error({
+                code: 'amount.invalid',
+                message: 'Bad amount. (only unsigned integers up to 2147483647)'
+            });
+        }
+        if (amount > account.balance) {
+            return request.error({
+                code: 'amount.outOfBalance',
+                message: 'Out of balance.'
+            });
+        }
+        let targetAccount = await AccountController.select({ where: { name: data.name } });
+        if (!targetAccount) {
+            return request.error({
+                code: 'name.notExists',
+                message: 'This account does not exists.'
+            });
+        }
+        account.update({
+            balance: account.balance - amount
+        });
+        targetAccount.update({
+            balance: targetAccount.balance + amount
+        });
+        return request.response({ success: true });
+    }
 }
