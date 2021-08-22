31b157cb6b1935ec7f6c22aa2783c8f12ea9aad0
ascented <toffuyume@gmail.com>
2021-08-22 03:27:05 +0300

    [API] added dotenv

--- api/security.ts
+++ api/security.ts
@@ -1,9 +1,13 @@
 import { Request } from "./controller";
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 
 export default class Security {
     public static developer(request: Request) {
-        if (request.data.developer_key != 'ascent') {
+        if (request.data.developer_key != process.env.DEVELOPER_KEY) {
             request.error({
                 code: 'security.restricted',
                 message: 'Restricted.'
