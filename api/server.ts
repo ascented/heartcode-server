6545238c3e75570a96442cd417638f9d62f8803c
ascented <toffuyume@gmail.com>
2021-08-24 14:31:46 +0300

    [API] controller use method rename

--- api/server.ts
+++ api/server.ts
@@ -6,10 +6,8 @@ import AccountController from './account';
 const app = express();
 const server = http.createServer(app);
 
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use('/', AccountController.router);
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/', AccountController.bind);
 
 server.listen(8080, () => console.log('Started'));
